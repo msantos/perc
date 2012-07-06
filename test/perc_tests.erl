22294806499de73f89e232649744e33a81c1ecde
Michael Santos <michael.santos@gmail.com>
2012-07-06 10:13:49 -0400

    User may not be allowed to decrease process priority

--- test/perc_tests.erl
+++ test/perc_tests.erl
@@ -1,21 +1,21 @@
 %% Copyright (c) 2012, Michael Santos <michael.santos@gmail.com>
 %% All rights reserved.
-%% 
+%%
 %% Redistribution and use in source and binary forms, with or without
 %% modification, are permitted provided that the following conditions
 %% are met:
-%% 
+%%
 %% Redistributions of source code must retain the above copyright
 %% notice, this list of conditions and the following disclaimer.
-%% 
+%%
 %% Redistributions in binary form must reproduce the above copyright
 %% notice, this list of conditions and the following disclaimer in the
 %% documentation and/or other materials provided with the distribution.
-%% 
+%%
 %% Neither the name of the author nor the names of its contributors
 %% may be used to endorse or promote products derived from this software
 %% without specific prior written permission.
-%% 
+%%
 %% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 %% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 %% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -67,8 +67,13 @@ prio_test() ->
 
     {error, eacces} = perc:setpriority(?PRIO_PROCESS, Pid, -10),
 
-    {ok, 5} = perc:renice({pid, Pid}, "-5"),
-    {ok, 15} = perc:renice({pid, Pid}, "+10"),
+    % User may not be allowed to decrease process priority
+    {ok, 15} = case perc:renice({pid, Pid}, "-5") of
+        {ok, 5} ->
+            perc:renice({pid, Pid}, "+10");
+        {error, eacces} ->
+            perc:renice({pid, Pid}, "+5")
+    end,
 
     ok = perc:kill(Pid, 9).
 
